c97802d2cdf16564fb742f3f4a9ed280997ac199
派大星的随波逐流 <148770289+pdxdsbzl@users.noreply.github.com>
2023-10-23 19:51:13 +0800

    Add files via upload

---
+++ 10.23.16.59.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	scanf("%d",&a);
+	if(a%2==1)
+	printf("是奇数");
+	else
+	printf("我干你呐");
+	int i=1;
+    while(i<=100)
+    {if(i%2==1)
+    printf("%d",i);
+    i++;
+	}
+	
+}
